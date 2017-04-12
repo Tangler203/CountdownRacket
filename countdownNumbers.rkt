fb48594dcb0a96a5c6daefbfdacf9a112939e1da
Liam Joyce <liamerjoycer@gmail.com>
2017-04-12 15:23:37 +0100

    added a few comments explaining stuff

--- countdownNumbers.rkt
+++ countdownNumbers.rkt
@@ -1,14 +1,15 @@
 #lang racket
 
-
+;Creates static list of numbers and operators
 (define l (list 1 4 '+ 3 '-))
-(define k (list (list 1 4 '+ 3 '-) (list 3 4 '+ 1 '-)))
 
 (define n (remove-duplicates(cartesian-product(permutations l))))
 
+; Reverse Polish Notation function, Sourced from https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Racket
+
 (define (calculate-RPN expr)
   (for/fold ([stack '()]) ([token expr])
-    ;(printf "~a\t -> ~a~N" token stack)
+    ;(printf "~a\t -> ~a~N" token stack) ; Uncomment to see workings, not recommended for long lists.
     (match* (token stack)
      [((? number? n) s) (cons n s)]
      [('+ (list x y s ___)) (cons (+ x y) s)]
@@ -19,10 +20,12 @@
      [(x s) n(error "calculate-RPN: Cannot calculate the expression:" 
                    (reverse (cons x s)))])))
 
+
+
 (define (sum x)
-  (if (null? x)
+  (if (null? x) ; checks if list is empty, if so end the function
       0
       ((writeln (calculate-RPN (car (car x)))) (sum (cdr x)))))
 
 
-'wait
+'ready
