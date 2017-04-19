4a87dc9d6b9c57555c6a91745bd51c840eff341d
Liam Joyce <liamerjoycer@gmail.com>
2017-04-19 12:31:33 +0100

    Removed double cars

--- countdownNumbers.rkt
+++ countdownNumbers.rkt
@@ -2,10 +2,12 @@
 
 ;Creates static list of numbers and operators
 (define l (list 1 4 '+ 3 '-))
-(define y (list 1 2 3 6))
-(define z (list 4 5 7))
 
-(define n (remove-duplicates(cartesian-product(permutations l))))
+(define l1 (list 1 2 3))
+(define l2 (list '+ '- '* '/))
+
+
+(define n (remove-duplicates(permutations l)))
 
 ; Reverse Polish Notation function, Sourced from https://rosettacode.org/wiki/Parsing/RPN_calculator_algorithm#Racket
 
@@ -27,15 +29,16 @@
 ;1 stack (r) for validating
 
 
-;Create stack r starting at .
+;Create stack r starting at 0.
 ;Pass through expression.
 ;If next element in expression is number, increase r by 1.
 ;If next element in expression is operand, check if r > 1.
 ;If r > 1, decrease r by 1 and move on.
 ;if r < 2, expression is not rpn-compatible. Move on to next expression
 ;Stack is rpn-compatible if r = 1.
-
-
+ 
+(define (express l)
+  (remove-duplicates(permutations (append l l2))))
 
 
 (define (valid-rpn? e[r 0])
@@ -51,10 +54,10 @@
 
 
 (define (sum x)
-  (if (null? x) ; checks if list is empty, if so end the function
+  (if (null? (car x)) ; checks if list is empty, if so end the function
       0
-      (if (valid-rpn? (car (car x)))
-          ((write (car (car x))) (writeln (calculate-RPN (car(car x))))  (sum (cdr x)) )
+      (if (valid-rpn? (car x))
+          ((write (car x)) (writeln (calculate-RPN (car x)))  (sum (cdr x)) )
           (sum (cdr x)))))
 
 'ready
